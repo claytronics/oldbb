4210e3904129c17bdc9daf65123380fdc5db1fdd
Pierre Thalamy <pierre.thalamy@gmail.com>
2014-04-25 10:43:20 -0400

    Re-enable sound

---
+++ build/src-bobby/hw-sim/hwAudio.c
@@ -0,0 +1,20 @@
+#ifndef __AUDIO_C__
+#define __AUDIO_C__
+
+#include "../hw-api/hwAudio.h"
+
+//dummy audio file
+
+void initHWAudio(){
+
+}
+
+void chirpHW(unsigned int x, unsigned int y){
+
+}
+
+void setDacHW(unsigned int x, unsigned int y){
+
+}
+
+#endif
