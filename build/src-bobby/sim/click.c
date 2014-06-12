52fed4468d931430a88431a1af75384845eb41a6
Pierre Thalamy <pierre.thalamy@gmail.com>
2014-06-12 17:22:57 -0400

    Add support for Mac OS X to BBsim, modify flags and sim source includes

--- build/src-bobby/sim/click.c
+++ build/src-bobby/sim/click.c
@@ -1,7 +1,13 @@
 /* - OpenGL Libraries - */
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <GLUT/glut.h>
+#else
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
+#endif
 
 #include "click.h"
 #include "render.h"
