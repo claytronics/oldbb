26e916996ab47ba89bb6e09eef7a6c3adeb15581
Pierre Thalamy <pierre.thalamy@gmail.com>
2014-07-25 16:47:29 +0200

    Write script to automate compilation

--- build/bin/compile-meld.sh
+++ build/bin/compile-meld.sh
@@ -1,8 +1,31 @@
 #!/bin/sh
 
-(cd cl-meld
+usage(){
+    echo "Usage: compile-meld.sh meldfile (without extension)"
+    echo "Exemple: compile-meld.sh LM-programs/ends2"
+    echo "Make sure to run . ./initbb.sh script beforehand"
+    exit 1
+}
+
+[[ $# -eq 0 ]] && usage
+
+(cd ../../../../cl-meld/
 echo "(load \"load\")
-(cl-meld:meld-compile \"../$1.meld\" \"../$1\")" | sbcl)
-echo "Compilation done - Copying file to parsing directory"
-cp $1.m modifiedVM/oldbb/build/src-bobby/meldinterp-runtime/samples
+(cl-meld:meld-compile \"$BBASE/apps/sample-meld/$1.meld\" \"$BBASE/apps/sample-meld/$1\")" | sbcl)
+echo "Compilation done"
+
+echo "Generating .bb file"
+LMParser $1.m
+
+if [ "$BB" == "block" ]; then
+  echo "Moving .bb file to arch-blocks"
+  mv $1.bb arch-blocks/meldinterp-runtime/ends.bb
+else
+  echo "Moving .bb file to arch-$ARCH"
+  mv $1.bb arch-$ARCH/meldinterp-runtime/ends.bb
+fi
+
+echo "Compiling the VM with the LM program"
+make
+
 echo "Done."
