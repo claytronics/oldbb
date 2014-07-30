db3657467f757a402a4bc4250ac7d2fcf7f92bd3
Pierre Thalamy <pierre.thalamy@gmail.com>
2014-07-30 15:41:36 +0200

    Document LMParser header as much as possible

--- build/src-bobby/meldinterp-runtime/LMParser.h
+++ build/src-bobby/meldinterp-runtime/LMParser.h
@@ -4,17 +4,35 @@
 #include "stdint.h"
 #include "string.h"
 
+/******************************************************************************
+@Description: LMParser is used to generate a .bb file (In this case, a Meld byte code file for the targetVM = oldbb's one), from a .m file (Byte code file from Flavio Cruz's cl-meld compiler, I sometimes refer it as source MeldVM).
+
+@author: Pierre Thalamy
+@email: pierre.thalamy@gmail.com
+@note: Feel free to contact me if you have any question.
+*******************************************************************************/
+
 typedef unsigned char byte;
 
 /* Used to check if opened file is Meld byte code file */
+/* First 8 bytes of any Meld byte code file */
 const uint32_t MAGIC1 = 0x646c656d;
 const uint32_t MAGIC2 = 0x6c696620;
 
-/* Macros from source MeldVM */
+/* Macros from source MeldVM
+ *
+/* Only version 0.11 is currently supported by the parser */
 #define VERSION_AT_LEAST(MAJ, MIN) \
 (majorVersion > (MAJ) || (majorVersion == (MAJ) && minorVersion >= (MIN)))
+/* Maximum length of a predicate name string */
 #define PRED_NAME_SIZE_MAX 32
+/* Maximum length of aggregate information */
+/* PTHY: I do not really know the use of this since it looks always empty */
 #define PRED_AGG_INFO_MAX 32
+
+
+/* Macros for target MeldVM */
+/* Size of a predicate descriptor, without argument descriptor */
 #define PREDICATE_DESCRIPTOR_SIZE 6
 
 /* Field types for target VM */
@@ -31,6 +49,9 @@ enum field_type {
    FIELD_FLOATLIST = 0xb,
    FIELD_NODELIST = 0xc
 };
+
+/* Structures used for storing information a rule and predicates, 
+ * which makes printing easier */
 
 typedef struct _Predicate {
   uint32_t codeSize; 		/* Size of byte code */
@@ -61,37 +82,48 @@ typedef struct _Rule {
 } Rule;
 
 
-/* Source properties */
-#define PRED_AGG 0x01
-#define PRED_ROUTE 0x02
-#define PRED_REVERSE_ROUTE 0x04
-#define PRED_LINEAR 0x08
-#define PRED_ACTION 0x10
-#define PRED_REUSED 0x20
-#define PRED_CYCLE 0x40
+/* Source predicate properties */
+#define PRED_AGG 0x01		/* Predicate is an aggregate */
+#define PRED_ROUTE 0x02		/* Predicate is used for communication */
+#define PRED_REVERSE_ROUTE 0x04	/* PTHY: Don't know the use of this one */
+#define PRED_LINEAR 0x08	/* Linear predicate */
+#define PRED_ACTION 0x10	/* Action predicate -- ignore it for targetVM */
+#define PRED_REUSED 0x20	/* Unused by targetVM */
+#define PRED_CYCLE 0x40		/* Unused by targetVM */
+/* If not linear, predicate is persistent */
 
-/* Target properties */
-#define TYPE_AGG 0x01
-#define TYPE_PERSISTENT 0x02
-#define TYPE_LINEAR 0x03
-#define TYPE_DELETE 0x08
-#define TYPE_SCHEDULE 0x10
-#define TYPE_ROUTING 0x20
-
+/* Source aggregate types */
+/* PTHY: It seems useless for targetVM */
 #define PRED_AGG_LOCAL 0x01
 #define PRED_AGG_REMOTE 0x02
 #define PRED_AGG_REMOTE_AND_SELF 0x04
 #define PRED_AGG_IMMEDIATE 0x08
 #define PRED_AGG_UNSAFE 0x00
 
-#define AGG_FIRST 1
-#define AGG_MAX_INT 2
-#define AGG_MIN_INT 3
-#define AGG_SUM_INT 4
-#define AGG_MAX_FLOAT 5
-#define AGG_MIN_FLOAT 6
-#define AGG_SUM_FLOAT 7
-#define AGG_SUM_LIST_FLOAT 11
-/* done */
+/* Target predicate properties */
+/* !-- TYPE = PREDICATE --! */
+#define TYPE_AGG 0x01		/* Predicate is an aggregate */
+#define TYPE_PERSISTENT 0x02	/* Predicate is persistent */
+#define TYPE_LINEAR 0x03	/* Predicate is linear */
+#define TYPE_DELETE 0x08	/* Unused */
+#define TYPE_SCHEDULE 0x10	/* Unused */
+#define TYPE_ROUTING 0x20	/* Unused */
+
+/* Target aggregate types */
+#define AGG_FIRST 1		/* First received? */
+#define AGG_MAX_INT 2		/* Maximum int */
+#define AGG_MIN_INT 3		/* Minimum int */
+#define AGG_SUM_INT 4		/* Sum of all int's */
+#define AGG_MAX_FLOAT 5		/* Max float */
+#define AGG_MIN_FLOAT 6		/* Min float */
+#define AGG_SUM_FLOAT 7		/* Sum of all floats */
+
+/* Compatibility with the targetVM may be limited for all these types
+ * If it turns out that they need to be used, some additional 
+ * conversion / implementation work may be needed */
+#define AGG_SET_UNION_INT 8    
+#define AGG_SET_UNION_FLOAT 9
+#define AGG_SUM_LIST_INT 10
+#define AGG_SUM_LIST_FLOAT 11	
 
 #endif	/* ifdef __PARSER_H__ */
