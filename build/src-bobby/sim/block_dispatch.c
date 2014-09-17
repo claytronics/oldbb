0035f494cbfa71df3f75f3b9ea3b72f49aa078a5
Seth Goldstein <seth@flashgroup.com>
2014-09-17 15:14:30 -0400

    add code to determine if a thread stops calling its blocktick.  This is for debugging on simulator

--- build/src-bobby/sim/block_dispatch.c
+++ build/src-bobby/sim/block_dispatch.c
@@ -1,9 +1,13 @@
 #include <pthread.h>
+#include <malloc.h>
+#include <string.h>
 
 #include "../system/myassert.h"
 #include "block.h"
 #include "block_dispatch.h"
 #include "unistd.h"
+#include "sim.h"
+#include "../hw-api/hwMemory.h"
 
 extern int blockProgram(void);
 extern void blockTick(void);
@@ -56,36 +60,164 @@ static void installThread(Block *b)
 	assert(ptr == NULL && pthread_setspecific(key, b) == 0);
 }
 
+int maxThis;
+Block** allThis = NULL;
+
 static void* launchBlockProgram(Block *b)
 {
-	installThread(b);
+  installThread(b);
 
-    pthread_mutex_lock(&printmutex);
-	fprintf(stderr, "Block id: %s\n", nodeIDasString(b->id, 0));
+  pthread_mutex_lock(&printmutex);
+  fprintf(stderr, "Block id: %s\n", nodeIDasString(b->id, 0));
+  if (b->id >= maxThis) {
+    maxThis = (maxThis+1)*2;
+    allThis = realloc(allThis, sizeof(maxThis)*sizeof(Block*));
+  }
+  allThis[b->id] = b;
+  pthread_mutex_unlock(&printmutex);
+
+  // call user program
+  int x = blockProgram();
+  int* xp = malloc(sizeof(int));
+  *xp = x;
+  pthread_exit((void*) xp);
+  return 0;
+}
+
+static int* destroyedBlocks = NULL;
+static unsigned int* tickCount = NULL;
+static unsigned int* prevTickCount = NULL;
+static int maxBlock = 0;
+static int maxId = 0;
+static byte checkMode = 0;
+
+// this block seems to have stopped calling blocktick
+void
+blockLocked(int x)
+{
+  pthread_mutex_lock(&printmutex);
+  fprintf(stderr, "blocktick not running properly! @ %d\nTICKS:", x);
+  int j;
+  for (j=0; j<maxId; j++) {
+    fprintf(stderr, " %4d", tickCount[j]);
+  }
+  fprintf(stderr, "\n PREV:");
+  for (j=0; j<maxId; j++) {
+    fprintf(stderr, " %4d", prevTickCount[j]);
+  }
+  fprintf(stderr, "\nDESTR:");
+  for (j=0; j<maxId; j++) {
+    fprintf(stderr, " %4d", destroyedBlocks[j]);
+  }
+  fprintf(stderr, "\n");
+  pthread_mutex_unlock(&printmutex);
+}
+
+// checkMode == 0 -> we haven't copied anything recently.  If copyflag == 1, then copy
+// checkMode == 1 -> we have copied old values, don't do anything unless copyflag == 0
+void
+saveTickCount(int copyflag)
+{
+  if (copyflag == checkMode) return;
+  pthread_mutex_lock(&printmutex);
+  if (copyflag == checkMode) {
     pthread_mutex_unlock(&printmutex);
-
-	// call user program
-	int x = blockProgram();
-
-	pthread_exit((void*) x);
-	return 0;
+    return;
+  }
+  fprintf(stderr, "STC: %d %d\n", copyflag, tickCount[1]);
+  int j;
+  for (j=1; j<maxId; j++) {
+    if (copyflag) {
+      prevTickCount[j] = tickCount[j];
+    } else {
+      prevTickCount[j] = tickCount[j] = 0;
+    }
+  }
+  checkMode = copyflag;
+  pthread_mutex_unlock(&printmutex);
 }
 
 // emulates the interrupt that triggers blockTick
-static void* callBlockTick(Block *b)
+static 
+void* callBlockTick(Block *b)
 {
-    // set-up the block references correctly
-	installThread(b);
+  // set-up the block references correctly
+  installThread(b);
 
-    // just keep looping and call blockTick
-    while(1)
-    {
-        usleep(1000);
-        blockTick();
+  // just keep looping and call blockTick
+  while(1) {
+    usleep(1000);
+
+    int id = getGUID();
+    if (id >= maxId) {
+      // make sure we have room to record the info about this block
+      pthread_mutex_lock(&printmutex);
+      maxId = id+1;
+      if (id >= maxBlock) {
+        int newmax = (id+1)*2;
+        tickCount = realloc(tickCount, newmax*sizeof(int));
+        prevTickCount = realloc(prevTickCount, newmax*sizeof(int));
+        memset(tickCount, 0, sizeof(int)*newmax);
+        memset(prevTickCount, 0, sizeof(int)*newmax);
+
+        destroyedBlocks = realloc(destroyedBlocks, newmax*sizeof(int));
+        memset(destroyedBlocks+maxBlock, 0, (newmax-maxBlock)*sizeof(int));
+        maxBlock = newmax;
+      }
+      fprintf(stderr, "REALLOC:%d, %d, %d\n", id, maxId, maxBlock);
+      pthread_mutex_unlock(&printmutex);
+    }
+    tickCount[id]++;
+
+    // now scan list and see if anyone is left behind
+    unsigned int base = tickCount[id];
+    //fprintf(stderr, "Assign base=%d @ %d\n", base, id);
+    int i;
+    for (i=1; i<maxId; i++) {
+      if (destroyedBlocks[i] == 1) continue;
+      unsigned int diff = (base > tickCount[i]) ? base-tickCount[i] : tickCount[i] - base;
+      if (diff > 400) {
+        // we must have been here before, compare old to new.  If all
+        // have changed, reset and start over.
+        int j;
+        for (j=1; j<maxId; j++) {
+          if (destroyedBlocks[j] == 1) continue;
+          if (tickCount[j] == prevTickCount[j]) {
+            // there was no change on this block
+            blockLocked(j);
+            break;
+          }
+        }
+        saveTickCount(0);
+      }
+
+      if (diff > 200) {
+        // save current counts
+        saveTickCount(1);
+        break;
+      }
     }
 
-	pthread_exit(0);
-	return 0;
+    if (this()->destroyed == 1) {
+      blockprint(stderr, "I AM DESTROYED!!\n");
+      // let neighbors know we are gone
+      //tellNeighborsDestroyed(this());
+      // and never do anything again
+      destroyedBlocks[id] = 1;
+      pauseForever();
+    }
+
+    blockTick();
+  }
+
+  pthread_exit(0);
+  return 0;
 }
 
 
+// Local Variables:
+// mode: c
+// tab-width: 8
+// indent-tabs-mode: nil
+// c-basic-offset: 2
+// End:
