114c9cdc9808560c4bfe5c246a1c27141994e6dc
Seth Goldstein <seth@flashgroup.com>
2014-09-17 15:15:08 -0400

    get rid of warnings

--- build/src-bobby/sim/sim.h
+++ build/src-bobby/sim/sim.h
@@ -1,6 +1,12 @@
 #ifndef _SIM_H_
 #define _SIM_H_
 
-void err(char *prompt, ...);
+#ifdef BBSIM
+
+# include <stdarg.h>
+  void err(char *prompt, ...);
+  void blockprint(FILE* f, char* fmt, ...);
+  void pauseForever(void);
+#endif
 
 #endif
