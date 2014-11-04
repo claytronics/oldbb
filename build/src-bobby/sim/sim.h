9a177965341996cb876b5056a40a780a6544cef5
Seth Goldstein <seth@flashgroup.com>
2014-11-04 10:24:28 -0500

    DEBUGPRINT and IFSIMDEBUG

--- build/src-bobby/sim/sim.h
+++ build/src-bobby/sim/sim.h
@@ -3,10 +3,22 @@
 
 #ifdef BBSIM
 
+extern int simDebugLevel;
+extern void (*onBlockTick)(void);
+
+#define DEBUGPRINT(level, ...) debugprint(level, stderr, __VA_ARGS__)
+#define IFSIMDEBUG(level) if (level <= simDebugLevel)
+
 # include <stdarg.h>
   void err(char *prompt, ...);
   void blockprint(FILE* f, char* fmt, ...);
+  void debugprint(int level, FILE* f, char* fmt, ...);
   void pauseForever(void);
+#else
+
+# define DEBUGPRINT(level, ...) 
+# define IFSIMDEBUG(level) if (0)
+
 #endif
 
 #endif
