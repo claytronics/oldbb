2242135d5377e38291e73f2e00aba6b6e678c7d0
Seth Goldstein <seth@flashgroup.com>
2014-12-08 14:22:12 -0500

    move block.h into ifdef

--- build/src-bobby/sim/sim.h
+++ build/src-bobby/sim/sim.h
@@ -1,31 +1,30 @@
 #ifndef _SIM_H_
-#define _SIM_H_
-
-#include "block.h"
-
-#ifdef BBSIM
-
-extern int simDebugLevel;
-extern void (*onBlockTick)(void);
-
-#define DEBUGPRINT(level, ...) debugprint(level, stderr, __VA_ARGS__)
-#define IFSIMDEBUG(level) if (level <= simDebugLevel)
-
-# include <stdarg.h>
-  void err(char *prompt, ...);
-  void blockprint(FILE* f, char* fmt, ...);
-  void debugprint(int level, FILE* f, char* fmt, ...);
-  void pauseForever(void);
-#else
-
-# define DEBUGPRINT(level, ...) 
-# define IFSIMDEBUG(level) if (0)
-
-#endif
-
-Block* port2block(Block* b, PRef p);
-Uid port2id(Block* b, PRef p);
+# define _SIM_H_
 
 
+# ifdef BBSIM
+
+#  include "block.h"
+
+   extern int simDebugLevel;
+   extern void (*onBlockTick)(void);
+   Block* port2block(Block* b, PRef p);
+   Uid port2id(Block* b, PRef p);
+
+#  define DEBUGPRINT(level, ...) debugprint(level, stderr, __VA_ARGS__)
+#  define IFSIMDEBUG(level) if (level <= simDebugLevel)
+
+#  include <stdarg.h>
+   void err(char *prompt, ...);
+   void blockprint(FILE* f, char* fmt, ...);
+   void debugprint(int level, FILE* f, char* fmt, ...);
+   void pauseForever(void);
+# else
+
+#  define DEBUGPRINT(level, ...) 
+#  define IFSIMDEBUG(level) if (0)
+#  define blockprint(f, fmt, ...)
+
+# endif
 
 #endif
