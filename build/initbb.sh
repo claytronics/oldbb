c9ffac371f4b48fc7d8c1674e6de3f218d9de9e4
Andre Naz <andre.naz01@gmail.com>
2016-11-14 14:40:41 +0100

    Fix a bug in the instructions for the environment installation (add command to go back to the previous folder).

--- build/initbb.sh
+++ build/initbb.sh
@@ -7,7 +7,7 @@ export BBASE=$(pwd)
 export ARCH=$(uname -m)-$(uname -s)
 if [ ! -e $BBASE/bin/arch-$ARCH ]; then
   echo "No predefined files for $ARCH.";
-  echo "You must mkdir $BBASE/bin/arch-$ARCH; cd src-bobby; make build; make install";
+  echo "You must mkdir $BBASE/bin/arch-$ARCH; cd src-bobby; make build; make install; cd ..";
   return -1;
 fi
 export PATH=$PATH:$BBASE/bin/arch-$ARCH:$BBASE/bin
